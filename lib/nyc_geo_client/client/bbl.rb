7f1aa3d10b5b0bb766e89043c2bb4ab3fbe6daa9
Edgar Gonzalez <edgargonzalez@gmail.com>
2014-02-18 15:40:57 -0500

    Adjust the bbl docs

--- lib/nyc_geo_client/client/bbl.rb
+++ lib/nyc_geo_client/client/bbl.rb
@@ -3,13 +3,13 @@ module NYCGeoClient
     # Defines methods related to branches
     module BBL
 
-      # Returns property level information.
+      # Returns property level information about a tax lot.
       #
       # @param borough [String] The borough in which the address is located
       # @param block [String] Tax block number
       # @param lot [String] Tax lot number
       # @return [Hashie::Mash]
-      # @example property level information
+      # @example property level information about a tax lot
       #   NYCGeoClient.bbl('manhattan','00233', '0004')
       # @format :json, :xml
       def bbl(borough, block, lot)
